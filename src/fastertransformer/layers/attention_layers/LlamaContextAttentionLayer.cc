705785173db2becec172f589c6fcc6592e42717a
sfc-gh-zhwang <flex.wang@snowflake.com>
2023-08-21 19:11:08 -0700

    commit

--- src/fastertransformer/layers/attention_layers/LlamaContextAttentionLayer.cc
+++ src/fastertransformer/layers/attention_layers/LlamaContextAttentionLayer.cc
@@ -141,6 +141,7 @@ void LlamaContextAttentionLayer<T>::forward(TensorMap*                output_ten
                               hidden_units_,  // k
                               qkv_buf_,
                               qkv_size /* n */);
+        printf("%f\n", qkv_buf_[0]);
         // cublas_wrapper_->Gemm(CUBLAS_OP_N,
         //                       CUBLAS_OP_N,
         //                       3 * local_hidden_units_,  // n
