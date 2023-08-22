ad3956e478a6bb034f39bf9d49e50a8b21e02db7
sfc-gh-zhwang <flex.wang@snowflake.com>
2023-08-21 22:41:10 -0700

    commit

--- src/fastertransformer/layers/attention_layers/LlamaContextAttentionLayer.cc
+++ src/fastertransformer/layers/attention_layers/LlamaContextAttentionLayer.cc
@@ -179,7 +179,7 @@ void LlamaContextAttentionLayer<T>::forward(TensorMap*                output_ten
                         c_buf,
                         m /* n */);
             sync_check_cuda_error();
-            cudaMemcpy(C, c_buf, sizeof(float) * m*n, cudaMemcpyDeviceToHost);
+            cudaMemcpy(C, a_buf, sizeof(float) * m*n, cudaMemcpyDeviceToHost);
             sync_check_cuda_error();
             for (int i=0; i<m; i++) {
                 for (int j=0; j<n; j++) {
