9e069729cbcbf2d68bd055858a8f5ffd151aaad0
sfc-gh-zhwang <flex.wang@snowflake.com>
2023-08-22 13:19:14 -0700

    commit

--- src/fastertransformer/kernels/repeat_kv_kernels.cu
+++ src/fastertransformer/kernels/repeat_kv_kernels.cu
@@ -40,6 +40,9 @@ __global__ void repeat_kv(T* dst, const T* src, const int kv_head_num, const int
             int q_dst = 3 * repeat_num * kv_head_num * size_per_head * token_id + repeat_num * size_per_head * head_id + size_per_head * r + inner_id;
             int q_src = (repeat_num + 2) * kv_head_num * size_per_head * token_id + repeat_num * size_per_head * head_id + size_per_head * r + inner_id;
             dst[q_dst] = src[q_src];
+
+            int k_dst = 3 * repeat_num * kv_head_num * size_per_head * token_id + repeat_num * kv_head_num * size_per_head + repeat_num * size_per_head * head_id + size_per_head * r + inner_id;
+            int k_src = (repeat_num + 2) * kv_head_num * size_per_head * token_id + repeat_num * kv_head_num * size_per_head + size_per_head * head_id + inner_id;
             // dst[3 * repeat_num * kv_head_num * size_per_head * token_id + head_id * size_per_head * r + inner_id] = src[(repeat_num + 2) * kv_head_num * size_per_head * token_num + head_id * size_per_head * r + inner_id];
             // dst[repeat_num * 3 * kv_head_num * size_per_head * token_num + repeat_num * kv_head_num * size_per_head + head_id * size_per_head * r + inner_id] = src[(repeat_num + 2) * kv_head_num * size_per_head * token_num + repeat_num * kv_head_num * size_per_head + head_id * size_per_head + inner_id];
             // dst[repeat_num * 3 * kv_head_num * size_per_head * token_num + 2 * repeat_num * kv_head_num * size_per_head + head_id * size_per_head * r + inner_id] = src[(repeat_num + 2) * kv_head_num * size_per_head * token_num + (repeat_num + 1) * kv_head_num * size_per_head + head_id * size_per_head + inner_id];
