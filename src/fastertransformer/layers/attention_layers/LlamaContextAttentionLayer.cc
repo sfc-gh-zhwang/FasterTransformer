6571282fad9a236a39371a6f1880ac4372aa8543
sfc-gh-zhwang <flex.wang@snowflake.com>
2023-08-23 10:38:11 -0700

    commit

--- src/fastertransformer/layers/attention_layers/LlamaContextAttentionLayer.cc
+++ src/fastertransformer/layers/attention_layers/LlamaContextAttentionLayer.cc
@@ -714,7 +714,7 @@ void LlamaContextAttentionLayer<T>::allocateBuffer(size_t batch_size, size_t seq
     const auto type_size = sizeof(T);
     qkv_buf_ = (T*)allocator_->reMalloc(qkv_buf_, type_size * 3 * batch_size * seq_len * local_hidden_units_, true);
     size_t local_qkv_size = local_hidden_units_ + 2 * local_kv_head_num_ * size_per_head_;
-    printf("%d %d %d %d\n", type_size, batch_size, seq_len, local_qkv_size);
+    printf("%d %d %d %d %d %d \n", type_size, batch_size, seq_len, local_hidden_units_, local_kv_head_num_, local_qkv_size);
     qkv_buf_tmp_ = (T*)allocator_->reMalloc(qkv_buf_tmp_, type_size * batch_size * seq_len * local_qkv_size, true);
     q_buf_2_ = (T*)allocator_->reMalloc(q_buf_2_, sizeof(T) * batch_size * seq_len * 3 * local_hidden_units_, true);
     k_buf_2_ = q_buf_2_ + batch_size * seq_len * local_hidden_units_;
