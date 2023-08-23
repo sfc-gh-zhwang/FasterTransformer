3175af06b360e2e0adcd27f465569b4e2456b0b9
sfc-gh-zhwang <flex.wang@snowflake.com>
2023-08-23 15:26:48 -0700

    commit

--- src/fastertransformer/models/llama/LlamaContextDecoder.cc
+++ src/fastertransformer/models/llama/LlamaContextDecoder.cc
@@ -595,6 +595,24 @@ void LlamaContextDecoder<T>::forward(std::unordered_map<std::string, Tensor>*   
         }
     }
 
+    if (l == 0) {
+        printf("%d %d: %d %d\n", l, ite, h_token_num, hidden_units_);
+        T *self_attn_output = new T[h_token_num * hidden_units_];
+        cudaMemcpy(self_attn_output, decoder_output, sizeof(T)*h_token_num * hidden_units_, cudaMemcpyDeviceToHost);
+        sync_check_cuda_error();
+        int k = 0;
+        for (int i=0; i<h_token_num; i++) {
+            for (int j=0; j<hidden_units_; j++) {
+                if (j < 32) {
+                    printf("%f ", (float)self_attn_output[k]);
+                }
+                k++;
+            }
+            printf("\n");
+        }
+        delete self_attn_output;
+
+    }
     if (use_shared_contexts) {
         invokeUnCompactOutputs(decoder_output,
                                compact_decoder_features_,
