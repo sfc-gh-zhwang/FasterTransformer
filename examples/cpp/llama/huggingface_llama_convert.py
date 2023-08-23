d482d096d22d3961982e04de14c2c5d55af1c759
sfc-gh-zhwang <flex.wang@snowflake.com>
2023-08-22 22:40:16 -0700

    commit

--- examples/cpp/llama/huggingface_llama_convert.py
+++ examples/cpp/llama/huggingface_llama_convert.py
@@ -74,7 +74,7 @@ def split_and_convert(args):
     # load position_embedding from rank 0
     # model = torch.load(ckpt_name)
     print(f'load model from {args.in_file}')
-    model = LlamaForCausalLM.from_pretrained(args.in_file, device_map='cpu')
+    model = LlamaForCausalLM.from_pretrained(args.in_file, device_map='auto')
     # config = AutoConfig.from_pretrained(args.in_file)
     # print(config)
     # w = {}
