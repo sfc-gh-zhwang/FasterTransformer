6327ac5cc6a055847fba2d28431034841a4b8bf6
sfc-gh-zhwang <flex.wang@snowflake.com>
2023-08-29 23:03:19 -0700

    commit

--- examples/cpp/llama/llama_triton_example.cc
+++ examples/cpp/llama/llama_triton_example.cc
@@ -313,7 +313,7 @@ int main(int argc, char* argv[])
     const int   gpu_count  = ft::getDeviceCount();
     std::cout << "gpu_count: " << gpu_count << std::endl;
     const int   world_size = node_num * gpu_count;
-    std::string ini_name   = argc >= 2 ? std::string(argv[1]) : "/data/FasterTransformer/examples/cpp/llama/llama_config.ini";
+    std::string ini_name   = argc >= 2 ? std::string(argv[1]) : "/notebooks/FasterTransformer/examples/cpp/llama/llama_config.ini";
 
     // step 1: Create model
     std::shared_ptr<AbstractTransformerModel> model            = AbstractTransformerModel::createLlamaModel(ini_name);
