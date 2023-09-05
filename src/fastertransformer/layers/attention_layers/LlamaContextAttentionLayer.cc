62557b62105f70f2dcc24e9e1410dcb0d56c803d
sfc-gh-zhwang <flex.wang@snowflake.com>
2023-09-04 22:08:45 -0700

    commit

--- src/fastertransformer/layers/attention_layers/LlamaContextAttentionLayer.cc
+++ src/fastertransformer/layers/attention_layers/LlamaContextAttentionLayer.cc
@@ -728,10 +728,9 @@ void LlamaContextAttentionLayer<T>::allocateBuffer(size_t batch_size, size_t seq
     qkv_buf_ = (T*)allocator_->reMalloc(qkv_buf_, type_size * 3 * batch_size * seq_len * local_hidden_units_, true);
     size_t local_qkv_size = local_hidden_units_ + 2 * local_kv_head_num_ * size_per_head_;
     qkv_buf_tmp_ = (T*)allocator_->reMalloc(qkv_buf_tmp_, type_size * batch_size * seq_len * local_qkv_size, true);
-    // q_buf_2_ = (T*)allocator_->reMalloc(q_buf_2_, sizeof(T) * batch_size * seq_len * 3 * local_hidden_units_, true);
-    q_buf_2_ = (T*)allocator_->reMalloc(q_buf_2_, sizeof(T) * batch_size * seq_len * local_qkv_size, true);
+    q_buf_2_ = (T*)allocator_->reMalloc(q_buf_2_, sizeof(T) * batch_size * seq_len * 3 * local_hidden_units_, true);
     k_buf_2_ = q_buf_2_ + batch_size * seq_len * local_hidden_units_;
-    v_buf_2_ = k_buf_2_ + batch_size * seq_len * local_kv_head_num_ * size_per_head_;
+    v_buf_2_ = k_buf_2_ + batch_size * seq_len * local_hidden_units_;
 
     // save memory usage when using fmha
     if (allocate_qk_buf) {
