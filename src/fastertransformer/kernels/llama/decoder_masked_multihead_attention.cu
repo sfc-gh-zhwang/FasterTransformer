8af8e0d1de9959c4f11b1c4655dd7eb42154cea3
sfc-gh-zhwang <flex.wang@snowflake.com>
2023-09-08 19:13:21 -0700

    commit

--- src/fastertransformer/kernels/llama/decoder_masked_multihead_attention.cu
+++ src/fastertransformer/kernels/llama/decoder_masked_multihead_attention.cu
@@ -14,8 +14,8 @@
  * limitations under the License.
  */
 
-#include "src/fastertransformer/kernels/decoder_masked_multihead_attention.h"
-#include "src/fastertransformer/kernels/decoder_masked_multihead_attention/decoder_masked_multihead_attention_template.hpp"
+#include "src/fastertransformer/kernels/llama/decoder_masked_multihead_attention.h"
+#include "src/fastertransformer/kernels/llama/decoder_masked_multihead_attention/decoder_masked_multihead_attention_template.hpp"
 #include "src/fastertransformer/kernels/decoder_masked_multihead_attention_utils.h"
 #include "src/fastertransformer/utils/cuda_bf16_wrapper.h"
 #include <assert.h>
