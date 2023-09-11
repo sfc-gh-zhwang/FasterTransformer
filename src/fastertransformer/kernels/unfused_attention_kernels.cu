839b558530741cab709013fe0fa20b0b43f52510
sfc-gh-zhwang <flex.wang@snowflake.com>
2023-09-11 00:31:21 -0700

    commit

--- src/fastertransformer/kernels/unfused_attention_kernels.cu
+++ src/fastertransformer/kernels/unfused_attention_kernels.cu
@@ -1702,14 +1702,17 @@ template<typename T>
 __global__ void transpose_4d_batch_major_k_cache(
     T* k_dst, const T* k_src, const int head_num, const int kv_head_num, const int size_per_head, const int seq_len, const int max_seq_len)
 {
-    const int     batch_id = blockIdx.y;
-    const int     head_id  = blockIdx.z;
-    constexpr int X_ELEMS  = (sizeof(T) == 4) ? 4 : 8;
-
+    const int     batch_id      = blockIdx.y;
+    const int     head_id       = blockIdx.z;
+    constexpr int X_ELEMS       = (sizeof(T) == 4) ? 4 : 8;
+    const int     head_n_rep    = head_num / kv_head_num;
+    if (head_id % head_n_rep != 0) {
+        return;
+    }
     auto key_src = reinterpret_cast<const uint4*>(k_src + batch_id * head_num * size_per_head * seq_len
                                                   + head_id * size_per_head * seq_len);
     auto key_dst = reinterpret_cast<uint4*>(k_dst + batch_id * kv_head_num * size_per_head * max_seq_len
-                                            + head_id * size_per_head * max_seq_len);
+                                            + head_id / head_n_rep * size_per_head * max_seq_len);
 
     const int out_idx             = blockIdx.x * blockDim.x + threadIdx.x;
     int       size_per_head_div_x = size_per_head / X_ELEMS;
@@ -1757,14 +1760,19 @@ template<typename T>
 __global__ void transpose_4d_batch_major_v_cache(
     T* v_dst, const T* v_src, const int head_num, const int kv_head_num, const int size_per_head, const int seq_len, const int max_seq_len)
 {
-    const int batch_id = blockIdx.y;
-    const int head_id  = blockIdx.z;
+    const int batch_id      = blockIdx.y;
+    const int head_id       = blockIdx.z;
+    const int head_n_rep    = head_num / kv_head_num;
+
+    if (head_n_rep % head_n_rep != 0) {
+        return;
+    }
 
     // 16 byte loads will handle "x" dimension
     auto val_src = reinterpret_cast<const uint4*>(v_src + batch_id * head_num * size_per_head * seq_len
                                                   + head_id * size_per_head * seq_len);
     auto val_dst = reinterpret_cast<uint4*>(v_dst + batch_id * kv_head_num * size_per_head * max_seq_len
-                                            + head_id * size_per_head * max_seq_len);
+                                            + head_id / head_n_rep * size_per_head * max_seq_len);
 
     // idx is over output dimension L * size_per_head / x for values
     const int idx = blockIdx.x * blockDim.x + threadIdx.x;
